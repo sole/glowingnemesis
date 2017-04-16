5a9423b712ca6172415c3f598ae3e0f857c66072
sole <listas@soledadpenades.com>
2017-04-16 16:16:44 +0100

    play and loop in playlist

--- renderer.js
+++ renderer.js
@@ -1,10 +1,12 @@
 let scanAndFilter = require('./scanAndFilter.js');
 
+var out, playlist, player;
+
 window.onload = function() {
 	
-	var out = document.getElementById('out');
-	var playlist = document.getElementById('playlist');
-	var player = document.getElementById('player');
+	out = document.getElementById('out');
+	playlist = document.getElementById('playlist');
+	player = document.getElementById('player');
 	
 	// make window a drag target area
 	dragAndDropify(window, (files) => {
@@ -27,12 +29,14 @@ window.onload = function() {
 		playlist.innerHTML = items;
 	});
 
+	// Set up player (i.e. the <audio> element) events
+	player.addEventListener('ended', playNextSong);
+
 	playlist.addEventListener('dblclick', (e) => {
-		console.log(e);
 		e.preventDefault();
 		let target = e.target;
 		if(target.dataset.src) {
-			playSong(target.dataset.src);
+			playSongAtNode(target);
 		}
 	});
 };
@@ -58,8 +62,41 @@ function dragAndDropify(target, onFiles) {
 	};
 }
 
+function playerHighlightRow(songNode) {
+	// TODO temporary, should go away once I have a proper playlist object
+	let playlistContainer = songNode.parentNode;
+	let items = Array.from(playlistContainer.childNodes);
+	items.forEach((item) => {
+		item.classList.remove('highlight');
+	});
+	songNode.classList.add('highlight');
+}
+
+function playSongAtNode(node) {
+	playSong(node.dataset.src);
+	playerHighlightRow(node);
+}
+
 function playSong(path) {
 	player.src = path;
 	player.play();
+}
+
+function playNextSong() {
+	// TODO this is very clumsy and temporary
+	let currentRow = document.querySelector('#playlist div.highlight');
+	let allRows = Array.from(playlist.querySelectorAll('div'));
+	let position = 0;
+	for(let i = 0; i < allRows.length; i++) {
+		let r = allRows[i];
+		if(r === currentRow) {
+			position = i;
+			break;
+		}
+	}
+
+	position = (position + 1) % allRows.length;
+	let nextRow = allRows[position];
+	playSongAtNode(nextRow);
 }
 
